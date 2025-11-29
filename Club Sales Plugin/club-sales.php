f400f1d29ae6a41098239aaeaf9273c6014c766d
Sanwal Bajwa <166105367+sanwalbajwa@users.noreply.github.com>
2025-11-29 18:11:53 +0500

    Fix issue in club-sales.php

--- Club Sales Plugin/club-sales.php
+++ Club Sales Plugin/club-sales.php
@@ -17,7 +17,7 @@ if (!defined('ABSPATH')) {
 }
  
 // Define plugin constants
-define('CS_VERSION', '2.0.0');
+define('CS_VERSION', '2.0.0'); 
 define('CS_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('CS_PLUGIN_URL', plugin_dir_url(__FILE__));
 
