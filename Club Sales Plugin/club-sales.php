e540c82aae0de743c509ed23282608f3a21d2c34
Sanwal Bajwa <166105367+sanwalbajwa@users.noreply.github.com>
2026-01-21 18:25:23 +0500

    Fix club-sales.php

--- Club Sales Plugin/club-sales.php
+++ Club Sales Plugin/club-sales.php
@@ -10,7 +10,7 @@ License: GPL v2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: club-sales
 */  
-  
+   
 // Prevent direct access to this file
 if (!defined('ABSPATH')) {
     exit; 
